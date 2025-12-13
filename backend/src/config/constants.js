ce4d3be3226cb860889bde48f34b2ed4d573a679
Nemoto Mailon <minatokiroisen77@gmail.com>
2025-12-13 23:11:25 +0900

    setting the backend folde and files structures

---
+++ backend/src/config/constants.js
@@ -0,0 +1,1 @@
+export const DB_NAME = "etpQY2QBbBfmyX5M"
---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv'
+import connectDB from  "./config/database.js"
+import app from './app.js'
+
+dotenv.config({
+    path: './.env'
+})
+
+const startServer = async() > {
+    
+    try {
+        await connectDB()
+
+        app.on("error", (error)) => {
+            console.log("ERROR", error)
+            throw error
+        }
+
+
+
+    } catch(error) {
+        
+    }
+}
