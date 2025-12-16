6908df4994224a481c98c989385196643049c424
Nemoto Mailon <minatokiroisen77@gmail.com>
2025-12-16 16:10:20 +0900

    build MVC api controller and models with routes

---
+++ backend/src/models/users.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+const usersSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8,
+        maxLength: 15
+    }
+},
+    {
+        timestamps: true,
+    }
+)
+
+export const User = mongoose.model("User", usersSchema)
